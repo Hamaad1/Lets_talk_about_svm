66bb4f7dbd88a61d411cb433691cce4fa0bd4c8f
Rocky <105061834+Hamaad1@users.noreply.github.com>
2024-10-10 18:14:48 +0200

    Update runmethod_SSVR.py

--- practicing_runmethod_SSVR/runmethod_SSVR.py
+++ practicing_runmethod_SSVR/runmethod_SSVR.py
@@ -9,13 +9,13 @@ sys.path.append(os.path.join(current_dir, 'practicing_runmethod_SSVR'))
 from preprocessing_SSVR import process_datasets, minmaxscaler
 from configuration1_SSVR import runSVR
 from Configuration2_SSVR import runSVR_C2
-# from Configuration3_SSVR import runSVR_C3
-# from Configuration4_SSVR import runSVR_C4
-# from Configuration5_SSVR import runSVR_C5
+from Configuration3_SSVR import runSVR_C3
+from Configuration4_SSVR import runSVR_C4
+from Configuration5_SSVR import runSVR_C5
 
 base_names = [ 'DSI1', 'DSI2']#, 'LIB1', 'LIB2', 'MAN1', 'MAN2', 'SAH1', 'TIE1', 'TUT1','TUT2', 'TUT3', 'TUT4', 'TUT5', 'TUT6', 'TUT7', 'UJI1', 'UTS1', 'GPR00' , 'GPR01' , 'GPR02', 'GPR03', 'GPR04', 'GPR05', 'GPR06', 'GPR07', 'GPR08', 'GPR09', 'GPR10', 'GPR11', 'GPR12', 'GPR13' ,'SOD01', 'SOD02', 'SOD03', 'SOD04', 'SOD05', 'SOD06', 'SOD07', 'SOD08', 'SOD09', 'UEXB1', 'UEXB2', 'UEXB3', 'UJIB1', 'UJIB2']
 max_iter = 5000000
-train_test_splits = [0.1, 0.15, 0.2, 0.25]#, 0.3, 0.35, 0.4, 0.45, 0.5
+train_test_splits = [0.1, 0.15, 0.2, 0.25]
 repetitions = 5
 
 config1 = {
@@ -37,7 +37,7 @@ config2 = {
     'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
     'gamma': 'scale',
     'epsilon': 0.001,
-    'degree': [2, 3],#, 4, 5],
+    'degree': [2, 3#, 4, 5],
     'tol': 0.001,
     'coef0' : [0,1,2,3],
     'train_test_splits': train_test_splits,
@@ -49,7 +49,7 @@ config2 = {
 config3 = {
     'C': 1.0,
     'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-    'gamma': ['scale', 'auto', 0.001],#, 0.01, 0.1, 1.0, 10],
+    'gamma': ['scale', 'auto', 0.001]
     'epsilon': 0.001,
     'degree': [2, 3, 4, 5],
     'tol': 0.001,
@@ -61,11 +61,11 @@ config3 = {
 }
 
 config4 = {
-    'C': [0.001, 0.01,0.1],#np.linspace(0.001, 1.0, num=6).tolist(),# + list(range(1, 101,2)),
+    'C': [0.001, 0.01,0.1, 10, 50,100]
     'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-    'gamma': ['scale', 'auto', 0.001],# 0.01, 0.1, 1.0, 10],#, 100, 0.0001, 0.00001, 0.000001, 0.0000001],
+    'gamma': ['scale', 'auto', 0.001]
     'epsilon': 0.001,
-    'degree': [2, 3],# 4, 5],
+    'degree': [2, 3 4, 5],
     'tol': 0.001,
     'coef0' : [0,1,2,3],
     'train_test_splits': train_test_splits,
@@ -76,9 +76,9 @@ config4 = {
 
 
 config5 = {
-    'C': [0.001, 0.01,0.1],#np.linspace(0.001, 1.0, num=2).tolist(),# + list(range(1, 100,5)),# + list(range(100, 1000, 10))#np.logspace(-3, 3, 649)
+    'C': [0.001, 0.01,0.1, 10, 50,100],
     'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-    'gamma': ['scale', 'auto', 0.001, 0.01],#, 0.1, 1.0, 10],
+    'gamma': ['scale', 'auto', 0.001, 0.01, 0.1, 1.0, 10],
     'epsilon': [0.001, 0.01, 0.1, 1.0] ,
     'degree': [2, 3, 4, 5],
     'tol': [0.0001, 0.001, 0.01],
@@ -181,15 +181,15 @@ def main():
 
             # Process the specific dataset with the selected configuration
             if config_choice == '1':
-                runSVR(data, user_choice, results_directory, dataset_params[base_name], config)#, dataset_params[user_choice]
+                runSVR(data, user_choice, results_directory, dataset_params[base_name], config)
             elif config_choice == '2':
-                runSVR_C2(data, user_choice, results_directory, dataset_params[base_name], config)#, dataset_params[user_choice]
+                runSVR_C2(data, user_choice, results_directory, dataset_params[base_name], config)
             elif config_choice == '3':
-                runSVR_C3(data, user_choice, results_directory, dataset_params[base_name], config)#, dataset_params[user_choice]
+                runSVR_C3(data, user_choice, results_directory, dataset_params[base_name], config)
             elif config_choice == '4':
-                runSVR_C4(data, user_choice, results_directory, dataset_params[base_name], config)#, dataset_params[user_choice]
+                runSVR_C4(data, user_choice, results_directory, dataset_params[base_name], config)
             elif config_choice == '5':
-                runSVR_C5(data, user_choice, results_directory, dataset_params[base_name], config)#, dataset_params[user_choice]
+                runSVR_C5(data, user_choice, results_directory, dataset_params[base_name], config)
 
 if __name__ == "__main__":
     main()
